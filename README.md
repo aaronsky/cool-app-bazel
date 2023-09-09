fe993cab3daf940c7845765bb6a2b66209714827
Aaron Sky <aaronsky@skyaaron.com>
2023-09-09 17:42:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cool App
+
+A cool app
---
+++ ios/app/src/Application.swift
@@ -0,0 +1,11 @@
+import FirebaseAnalyticsSwift
+import ios_root_view
+import SwiftUI
+
+@main struct WaniKaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
