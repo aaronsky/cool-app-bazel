c1bcca1bebf84bf170749c4232a927f4295b3a04
Aaron Sky <aaronsky@skyaaron.com>
2023-09-10 07:58:03 -0400

    fix tests

--- ios/root_view/test/AppModuleTests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-@testable import AppModule
-
-@MainActor
-class AppModuleTests: XCTestCase {
-}
---
+++ ios/root_view/test/AppViewTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+@testable import ios_root_view
+
+@MainActor
+class AppViewTests: XCTestCase {
+}
